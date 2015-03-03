e078a8452ba9a94bd4434b48fb6d2e570a808c37
Ashwin Srinath <ashwinsrnth@gmail.com>
2015-03-03 06:24:00 -0500

    add demo and ica to gh-pages

---
+++ code/demo.py
@@ -0,0 +1,13 @@
+import numpy as np
+from scipy import linalg
+
+from ica import fastica
+
+def test():
+    data = np.random.random((5000, 100))
+    u, s, v = linalg.svd(data)
+    pca = np.dot(u[:, :10].T, data)
+    results = fastica(pca.T, whiten=False)
+
+if __name__ == '__main__':
+    test()
